d96247a42bb505039d7e9c3677c28cb11de3d971
klee-arc <kenfjy@gmail.com>
2015-03-13 11:00:16 +0900

    added config.js

---
+++ src/octoprint/static/js/config.js
@@ -0,0 +1,11 @@
+// host name of fabrica
+host_name = "localhost:3000";
+
+// ID number that has been issued from fabrica
+fabrica_id = ;
+
+// Secret key number issued from Octoprint
+api_key = ;
+
+// interval time you would like to make your Fabrica to check
+intervalMillis = ;
